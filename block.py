94a0fe363a8e6bb4e6b2c9bb69feff0952b47a24
olympia <owojcik7@gmail.com>
2020-03-12 11:32:40 -0400

    Add mine_block method and genesis block to block.py

--- block.py
+++ block.py
@@ -1,17 +1,51 @@
+
+import time 
+
+def mine_block(last_block, data):
+    """
+    Mine a block based on the given last_block and data
+    """
+    timestamp = time.time_ns() #returns unique num based on counting num of nanoseconds since jan 1 1970
+    last_hash = last_block.hash #found by looking at hash of last block
+    hash = f'{timestamp}-{last_hash}' #temp implementation of hash
+    
+    return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+    """
+    Generate the genesis block, hard coded w/ data as empty list
+    """
+    return Block(1, 'genesis_last_hash', "genesis_hash", [])
+
 class Block: 
     """
     Block: a unit of storage
     Store transactions in a blockchain that supports a cryptocurrency
     """
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp 
+        self.last_hash = last_hash 
+        self.hash = hash
         self.data = data 
+
     def __repr__(self):
-        return f'Block - data: {self.data}'
+        '''
+        String representation of the Block class
+        '''
+        return ('Block('
+            f'timestamp: {self.timestamp},'
+            f'last_hash: {self.last_hash},'
+            f'hash: {self.hash},'
+            f'data: {self.data})')
 
 def main():
-    block = Block('foo')
+    # block = Block('foo')
+    # print(block)
+    # print(f'block.py__name__: {__name__}')
+
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
     print(block)
-    print(f'block.py__name__: {__name__}')
 
 
 if __name__ == "__main__":
