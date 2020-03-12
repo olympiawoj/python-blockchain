b07aa27277fb77d5df074780937252ae43908b57
olympia <owojcik7@gmail.com>
2020-03-12 11:06:36 -0400

    Extract Block class into its own block.py module and use __main__ for testing

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,4 @@
-class Block: 
-    """
-    Block: a unit of storage
-    Store transactions in a blockchain that supports a cryptocurrency
-    """
-    def __init__(self, data):
-        self.data = data 
-    def __repr__(self):
-        return f'Block - data: {self.data}'
+from block import Block
 
 class Blockchain: 
     """
@@ -24,13 +16,19 @@ class Blockchain:
     def add_block(self, data):
         self.chain.append(Block(data)) #make instance of block and append to chain
 
-#make instance of Blockchain
-blockchain = Blockchain()
-# add blocks of data
-blockchain.add_block("one")
-blockchain.add_block("two")
+def main():
+    #make instance of Blockchain
+    blockchain = Blockchain()
+    # add blocks of data
+    blockchain.add_block("one")
+    blockchain.add_block("two")
 
-#output is memory address ofblockchain object we created, under hood python gives every object it's own address that way it can keep track of your object as it executes your code
-print(blockchain)
+    #output is memory address ofblockchain object we created, under hood python gives every object it's own address that way it can keep track of your object as it executes your code
+    print(blockchain)
 
 
+    print(f'blockchain.py__name__: {__name__}')
+
+
+if __name__ == "__main__":
+    main()
